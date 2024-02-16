e2b8cab12b1c681363ca77e4302f3936fcbafc0e
Gabi <gabriellebetto@gmail.com>
2024-02-16 19:51:38 -0300

    Aulas de 2024

---
+++ Atividade_dia_15/main.c
@@ -0,0 +1,12 @@
+/*Dado um número digitado pelo usuário, busque dados divisíveis por 3, 5 e ambos de 0 ao número. Vetor para 3; para 5; para 3 e 5.
+Faça um código que calcule a média harmônica de 3 notas (lidas zero).
+Construa um código capaz de juntar duas strings (tamanho 10 cada) numa nova de tamanho 25.
+Dada uma string, garanta que ela sempre inicie com letra maiúscula.
+tabela ascii comparar
+Dada uma string, garanta que haverá no max um espaço entre palavras.
+Sem bibliotecas extras(String.h)
+tam = scanf
+vetor [tam/2]
+0 -> 10
+3 6 9
+*/
---
+++ Atividade_dia_16/main.c
@@ -0,0 +1,8 @@
+/*Correção de String 
+ Deseja - se que ao inserir uma frase no sistema seja feito: 
+Deixar todas as letras minúsculas;
+Começar com letra, ou seja, se inicia com espaços, retirar todos
+Remover quaisquer repetições de espaço como:
+A           Vida -> A vida
+Deixar a primeira letra maiúscula da frase.
+*/
